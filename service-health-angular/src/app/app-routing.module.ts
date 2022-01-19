bedcce7582e50169d96882aa6b3ad75ee301c247
Dennis Carrasquillo <denniscarrasquillo@Lindsays-MacBook-Air.local>
2022-01-19 14:25:34 -0500

    Testing iframe functionality

--- service-health-angular/src/app/app-routing.module.ts
+++ service-health-angular/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ServiceHealthComponent } from './service-health/service-health.component';
+import { HealthDashboardComponent } from './health-dashboard/health-dashboard.component';
 
 const routes: Routes = [
-  { path: 'service-health', component: ServiceHealthComponent }
+  { path: 'service-health', component: ServiceHealthComponent },
+  { path: 'health', component: HealthDashboardComponent }
 ];
 
 @NgModule({
--- service-health-angular/src/app/health-dashboard/health-dashboard.component.ts
+++ service-health-angular/src/app/health-dashboard/health-dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-health-dashboard',
@@ -11,10 +11,15 @@ export class HealthDashboardComponent implements OnInit {
 
   name = "ServiceHealth";
   ServiceHealthHtml: any;
+  urlString: string = "https://pepboys-service-health.s3.amazonaws.com/19-1-2022-13%3A11%3A4_Service-Health_Report.html";
+  urlSafe!: SafeResourceUrl;
 
   constructor(private http: HttpClientModule,private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+
+    this.urlSafe = this.sanitizer.bypassSecurityTrustHtml(this.urlString);
+
   }
 
 }
